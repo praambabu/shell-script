4a4ac57cf62d299f7322db0903d6462350ede11b
ram <146025924+praambabu@users.noreply.github.com>
2025-11-28 23:00:19 +0530

    Create Adhoc-command.sh

---
+++ Adhoc-command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PING_COUNT=3
+PING_TIMEOUT=1
+
+read -p "Enter Adhoc command: " cmd
+
+for i in $(cat /root/shell-script/servers.txt)
+do
+    ping -c "$PING_COUNT" -W "$PING_TIMEOUT" "$i" > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then    
+        echo $i 
+        echo -e "\e[31m `ssh $i $cmd`\e[0m"
+    else    
+        echo $i
+        echo -e "\e[31mError: SSH connection issue or remote server not pingging.\e[0m"
+        
+    fi
+done
