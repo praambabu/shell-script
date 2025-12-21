a841dc17fda4f9942513aac2d704ceb5c9a42742
ram <146025924+praambabu@users.noreply.github.com>
2025-12-21 17:48:31 +0530

    Update Adhoc-command.sh

--- Adhoc-command.sh
+++ Adhoc-command.sh
@@ -2,16 +2,21 @@
 PING_COUNT=3
 PING_TIMEOUT=1
 
-read -p "Enter Adhoc command: " cmd
+read -p "Enter Adhoc command: " command
+
+if [ -z "$command" ]; then
+    echo "Error: Input cannot be empty. Exiting."
+    exit 1
+fi
 
 for i in $(cat /root/shell-script/servers.txt)
 do
     ping -c "$PING_COUNT" -W "$PING_TIMEOUT" "$i" > /dev/null 2>&1
 
-    if [ $? -eq 0 ]; then    
+    if [ $? -eq 0 ]; then
         echo $i 
-        echo -e "\e[31m `ssh $i $cmd`\e[0m"
-    else    
+        echo -e "\e[31m `ssh $i $command`\e[0m"
+    else
         echo $i
         echo -e "\e[31mError: SSH connection issue or remote server not pingging.\e[0m"
         
